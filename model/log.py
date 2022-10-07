b992d4b97c12414d34ffba820a387aafd79d0d7a
Darrensiriram <ds.siriram@gmail.com>
2022-10-08 00:23:01 +0200

    created log model

---
+++ model/log.py
@@ -0,0 +1,18 @@
+
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional, Set
+
+from interface.IModel import IModel
+from model.user import User
+
+@dataclass
+class Log(IModel):
+
+    message: str
+    user: Optional[User] = field(default=None)
+    id: Optional[int] = field(default=None)
+    created_at: datetime = field(default_factory=datetime.now)
+
+    def getId(self) -> int:
+        return self.id or -1
